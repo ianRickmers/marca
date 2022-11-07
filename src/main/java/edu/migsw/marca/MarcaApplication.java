bf898eaf70ecdf9456dabadb75038a8b36f65fa2
ianRickmers <ian.rickmers@usach.cl>
2022-11-07 13:44:43 -0300

    Creación microservicio marcas

---
+++ src/main/java/edu/migsw/marca/MarcaApplication.java
@@ -0,0 +1,13 @@
+package edu.migsw.marca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcaApplication.class, args);
+	}
+
+}
